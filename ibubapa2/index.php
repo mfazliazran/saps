4aa13400ba4a7c9222632c440cb7e5ce17bbe739
Mohd Fazli Azran <mfazliazran@gmail.com>
2018-07-09 18:01:31 +0800

    Add files via upload

---
+++ ibubapa2/index.php
@@ -0,0 +1,31 @@
+<style>
+body {
+    background: #273d88;
+    font-family: 'Arvo',serif;
+    font-size: 16px;
+    margin: 0;
+    padding: 0;
+}
+
+div.header {
+    background: #fff;
+    width: 100%;
+}
+
+div {
+    display: block;
+align: center;
+}
+</style>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Semakan Ibu Bapa</title>
+</head>
+<body>
+<div id="header">
+/*<img src ="tutup.jpg"/>*/
+</div>
+</body>
+</html>
